948c6aecd2de721dff65e8c659def6c2a4bb9e16
mosikh98 <you@example.com>
2026-08-07 12:44:54 +0330

    rebrand: change applicationId to com.mosikh98.v2m for unique Play Store listing

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.mosikh98.v2m"
         minSdk = 24
         targetSdk = 37
         versionCode = 742
--- V2rayNG/app/src/main/java/com/v2ray/ang/util/Utils.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/util/Utils.kt
@@ -458,7 +458,7 @@ object Utils {
      *
      * @return True if the package is Xray, false otherwise.
      */
-    fun isXray(): Boolean = BuildConfig.APPLICATION_ID.startsWith("com.v2ray.ang")
+    fun isXray(): Boolean = true // این فورک همیشه از هسته Xray استفاده می‌کنه
 
     /**
      * Converts an InetAddress to its long representation
